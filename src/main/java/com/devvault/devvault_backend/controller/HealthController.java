03c902bc5b7d5a62e8783633c301296649ebd781
Yash <107048847+yash-1558@users.noreply.github.com>
2025-10-16 03:57:56 +0530

    Added system health endpoint

---
+++ src/main/java/com/devvault/devvault_backend/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.devvault.devvault_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Up";
+    }
+}
