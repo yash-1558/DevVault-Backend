eaa4f19c3f722ab71d3acfc3f1d3697eb145ccbd
Yash <107048847+yash-1558@users.noreply.github.com>
2025-10-16 03:20:52 +0530

    Initial Dev-vault project setup

---
+++ src/main/java/com/devvault/devvault_backend/DevvaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devvault.devvault_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevvaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevvaultBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devvault/devvault_backend/DevvaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devvault.devvault_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevvaultBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
